14c8439ba42407f4ba6781a632ae2f5f1332aa20
David Krause <krause@biochem2.uni-frankfurt.de>
2020-12-26 17:39:30 +0100

    fix config location; api constant
    Signed-off-by: David Krause <krause@biochem2.uni-frankfurt.de>

--- renewIP.nim
+++ renewIP.nim
@@ -2,22 +2,24 @@ import crosscheck
 import httpclient, json
 import parsecfg
 import strutils
+import os
+var config = loadConfig(getAppDir() / "conf.ini")
 
-var config = loadConfig("conf.ini")
+const api = "https://api.cloudflare.com/client/v4/zones/"
 
-let email = config.getSectionValue("", "email") 
-let apiKey = config.getSectionValue("", "apiKey") 
-let zoneKey = config.getSectionValue("", "zoneKey") 
-let domains = config.getSectionValue("", "domains").split(",") 
+let email = config.getSectionValue("", "email")
+let apiKey = config.getSectionValue("", "apiKey")
+let zoneKey = config.getSectionValue("", "zoneKey")
+let domains = config.getSectionValue("", "domains").split(",")
 
 let client = newHttpClient()
-client.headers = newHttpHeaders({ 
+client.headers = newHttpHeaders({
   "X-Auth-Email": email,
-  "X-Auth-Key": apiKey 
+  "X-Auth-Key": apiKey
   })
 
-proc getDnsRecords(): JsonNode = 
-  let body = client.request("https://api.cloudflare.com/client/v4/zones/" & zoneKey & "/dns_records", httpMethod = HttpGet).body #,  body = $body)
+proc getDnsRecords(): JsonNode =
+  let body = client.request(api & zoneKey & "/dns_records", httpMethod = HttpGet).body #,  body = $body)
   let j = parseJson(body)
   return j["result"]
 
@@ -27,7 +29,7 @@ proc getIdByName(name: string): string =
       return entry["id"].getStr()
 
 proc printDnsRecords() =
-  let body = client.request("https://api.cloudflare.com/client/v4/zones/" & zoneKey & "/dns_records", httpMethod = HttpGet).body
+  let body = client.request(api & zoneKey & "/dns_records", httpMethod = HttpGet).body
   let j = parseJson(body)
   # return j["result"]
   for site in j["result"]:
@@ -44,8 +46,8 @@ proc renewIP(name, id, ip: string) =
     "ttl": 1,
     "proxied":false
   }
- 
-  discard repr client.request("https://api.cloudflare.com/client/v4/zones/" & zoneKey & "/dns_records/" & id, httpMethod = HttpPut, body = $body)
+
+  discard repr client.request(api & zoneKey & "/dns_records/" & id, httpMethod = HttpPut, body = $body)
   # TODO: Check if update succesful ;)
 
 when isMainModule:
@@ -57,7 +59,7 @@ when isMainModule:
 
   let ip = getExternalIP(sites)
   echo "My external ip: ", ip
-  
+
   for domain in domains:
     echo "Updateing: ", domain.strip()
     let id = getIdByName(domain.strip())
